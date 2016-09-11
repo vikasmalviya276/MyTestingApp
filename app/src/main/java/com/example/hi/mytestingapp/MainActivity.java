586bb2dbfc1057b72330e5718b294f97ed477be9
vikasmalviya276 <vikas.malviya276@gmail.com>
2016-09-11 14:11:54 +0530

    Disaply Message

--- app/src/main/java/com/example/hi/mytestingapp/MainActivity.java
+++ app/src/main/java/com/example/hi/mytestingapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.hi.mytestingapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        messageDisplay();
+    }
+
+    private void messageDisplay() {
+        Toast.makeText(MainActivity.this, "Message", Toast.LENGTH_SHORT).show();
     }
 }
